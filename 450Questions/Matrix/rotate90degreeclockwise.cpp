662b025bd24f427b16e2cc8b1b27ece118e48945
jhonsnow456 <thakuraman22july@gmail.com>
2021-10-30 18:37:48 +0530

    rotate 90 degree clockwise completed

---
+++ 450Questions/Matrix/rotate90degreeclockwise.cpp
@@ -0,0 +1,43 @@
+// https://www.geeksforgeeks.org/rotate-a-matrix-by-90-degree-in-clockwise-direction-without-using-any-extra-space/
+
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+const int n = 4;
+
+void print(int arr[n][n]){
+    for (int i = 0; i < n; i++)
+        for (int j = 0; j < n; j++)
+            cout << arr[i][j] << " ";
+    cout << endl;
+}
+
+void rotate90clockwise(int arr[n][n]){
+    for (int i = 0; i < n; i++)
+        for (int j = i + 1; j < n; j++)
+            swap(arr[i][j], arr[j][i]);
+
+    for (int i = 0; i < n; i++){
+        int low = 0, high = n - 1;
+        while (low < high){
+            swap(arr[i][low], arr[i][high]);
+            low++; high--;
+        }
+    }
+}
+
+int main(){
+    int mat[n][n] = { 
+        { 1, 2, 3, 4 },
+        { 5, 6, 7, 8 },
+        { 9, 10, 11, 12 },
+        { 13, 14, 15, 16 } 
+    };
+
+    rotate90clockwise(mat);
+    print(mat);
+    
+    return 0;
+}
