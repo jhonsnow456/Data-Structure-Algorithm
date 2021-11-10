b9910addfa582512466d9f1087d55cbc78901333
jhonsnow456 <thakuraman22july@gmail.com>
2021-11-11 03:27:24 +0530

    Longest Palindrome created

---
+++ 450Questions/String/LongestPalindrome.cpp
@@ -0,0 +1,17 @@
+// https://practice.geeksforgeeks.org/problems/longest-palindrome-in-a-string3411/1
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+string longestPallindrome(string s){
+    
+
+}
+
+int main(){
+    string S = "vnrtysfrzrmzlygfv";
+        cout << longestPallindrome(S) << endl;        
+    return 0;
+}
