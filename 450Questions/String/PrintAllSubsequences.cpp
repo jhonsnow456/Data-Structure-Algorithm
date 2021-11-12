997cd5591d28d609909171ed92cabba76f21e14b
jhonsnow456 <thakuraman22july@gmail.com>
2021-11-12 15:21:38 +0530

    Print All Subsequence completed

---
+++ 450Questions/String/PrintAllSubsequences.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <unordered_set>
+
+using namespace std;
+
+
+// TIME COMPLEXITY: O(2^N)
+unordered_set<string> res;
+
+void generateSubsequence(string input, string output){
+    if (input.empty()){
+        res.insert(output);
+        return;
+    }
+    generateSubsequence(input.substr(1), output);
+    generateSubsequence(input.substr(1), output + input[0]);
+}
+
+void printSubsequences(string s){
+    generateSubsequence(s, "");
+    for (auto i: res)
+        cout << i << " ";
+    cout << endl;             
+}
+
+
+int main(){
+    string s = "aabc";
+        printSubsequences(s);
+    return 0;
+}
