3c8b84723ca300c9fc22c550d328a0e44718d0cc
Aman Thakur <54764701+jhonsnow456@users.noreply.github.com>
2022-01-22 01:43:54 +0530

    micellanious DSA problems

---
+++ DSA's Important/FindChainInLinkedList.cpp
@@ -0,0 +1,76 @@
+/*
+    यावद्बध्दो मरुद देहे यावच्चित्तं निराकुलम्।
+    यावद्द्रॄष्टिभ्रुवोर्मध्ये तावत्कालभयं कुत: ॥
+*/
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+struct Node{
+	int data;
+	Node* next;
+
+	Node (int data){
+		this->data = data;
+		this->next = NULL;
+	}
+};
+
+void push_back(Node* &head, int data){
+	Node* node = new Node(data);
+
+		if (head == NULL){
+			head = node;
+			return;
+		}else{
+			Node* temp = head;
+			while (temp->next != NULL)
+				temp = temp->next;
+
+			temp->next = node;
+		}
+}
+
+void display(Node* const head){
+	Node* temp = head;
+
+		while (temp != NULL){
+			cout << temp->data << ",";
+			temp = temp->next;
+		}
+}
+
+bool containCycle(Node* const head){
+	unordered_map<Node*, bool> hash_table;
+	Node *temp = head;
+
+		while (temp != NULL){
+			if (hash_table.count(temp) != 0) return true;
+			hash_table[temp] = true;
+			temp = temp->next;
+		}
+
+	return false;
+}
+
+int32_t main(){
+	// write the code here
+	Node *temp = NULL;
+
+		push_back(temp, 1);
+		push_back(temp, 2);
+		push_back(temp, 3);
+		push_back(temp, 4);
+		push_back(temp, 5);
+
+		display(temp);
+		cout << endl;
+
+	Node* node = temp->next->next->next->next;
+	node->next = temp->next->next; 
+
+		cout << ((containCycle(node) ? "Cycle Exists!!" : "Cycle don't Exists!!")) << endl;
+	
+	return 0;
+}
