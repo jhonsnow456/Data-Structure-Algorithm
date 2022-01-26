8d9dae1cebd1e9e98938cd091ea239e6c81b38e1
Aman Thakur <54764701+jhonsnow456@users.noreply.github.com>
2022-01-26 15:45:59 +0530

    Create practice.cpp

---
+++ CP-SNIPPET/practice.cpp
@@ -0,0 +1,13 @@
+/*
+    यावद्बध्दो मरुद देहे यावच्चित्तं निराकुलम्।
+    यावद्द्रॄष्टिभ्रुवोर्मध्ये तावत्कालभयं कुत: ॥
+*/
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int32_t main(){
+	// write the code here
+	return 0;
+}
