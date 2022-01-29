c99ca4184616a29fb5719a68f309b1d91953bf11
Aman Thakur <54764701+jhonsnow456@users.noreply.github.com>
2022-01-29 14:38:35 +0530

    Update Tokenization.cpp

--- DSA's Important/String/Tokenization.cpp
+++ DSA's Important/String/Tokenization.cpp
@@ -7,6 +7,8 @@
 
 using namespace std;
 
+
+// Method 1: Using String stream
 void stringStream(string &input){
 	stringstream tokenstream(input);
 
@@ -20,12 +22,48 @@ void stringStream(string &input){
 			cout << token << ", ";
 }
 
+// Method 2: Using strtok
 void stringToken(char* input){
 	char* token = strtok(input, " ");
 
 	while (token != NULL){
 		cout << token << ", ";
 		token = strtok(NULL, " ");
+	}
+}
+
+
+// Method 3: Using my string own tokenzation function
+char* strtoken(char* str, char delimiter){
+	static char *input = NULL;
+
+		if (str != NULL) input = str;
+		if (input == NULL) return NULL;
+
+	char *token = new char[strlen(input) + 1];
+	int i = 0;
+
+		for (; input[i] != '\0'; i++)
+			if (input[i] != delimiter) 
+				token[i] = input[i];
+			else{
+				token[i] = '\0';
+				input = input + i + 1;
+				return token;
+			}
+
+		token[i] = '\0';
+		input = NULL;
+
+	return token;
+}
+
+void stringTokenenization(char* input){
+	char* token = strtoken(input, ' ');
+
+	while (token != NULL){
+		cout << token << ", ";
+		token = strtoken(NULL, ' ');
 	}
 }
 
@@ -38,14 +76,18 @@ int32_t main(){
 
 		cout << endl;
 		
-	char input1[1000];
-	// cin.getline(input1, 1000);
-	int itr = 0;
+	char input1[input.length() + 1];
 
-		for (char c: input)
-			input1[itr++] = c;
-
+		strcpy(input1, input.c_str());
 		stringToken(input1);
+
+		cout << endl;
+
+
+	char input2[input.length() + 1];
+
+		strcpy(input2, input.c_str());
+		stringTokenenization(input2);
 
 	return 0;
 }
