20aedd61a75362ca3ab85d9bd9635664c8bf8e8b
Aman Thakur <54764701+jhonsnow456@users.noreply.github.com>
2022-02-07 22:55:54 +0530

    Create QuickSelect.cpp

---
+++ DSA's Important/04_Sorting&Searching/QuickSelect.cpp
@@ -0,0 +1,57 @@
+/*
+    यावद्बध्दो मरुद देहे यावच्चित्तं निराकुलम्।
+    यावद्द्रॄष्टिभ्रुवोर्मध्ये तावत्कालभयं कुत: ॥
+*/
+
+// https://www.geeksforgeeks.org/quickselect-algorithm/
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int partition(vector<int> &array, int left, int right){
+	int pivot = array[right], prev = left;
+
+		for (int next = left; next <= right - 1; next++){
+			if (array[next] <= pivot){
+				swap(array[prev], array[next]);
+				prev++;
+			}
+		}
+
+		swap(array[prev], array[right]);
+
+	return prev;
+}
+
+int kthsmallest(vector<int> &array, int left, int right, int k){
+	if (0 < k <= (right - left + 1)){
+
+		int index = partition(array, left, right);
+
+		if (index - left == k - 1)
+			return array[index];
+
+		if (index - left > k - 1)
+			return kthsmallest(array, left, index - 1, k);
+
+		return kthsmallest(array, index + 1, right, k - index + left - 1);
+	}
+
+	else return INT_MAX;
+}
+
+int32_t main(){
+	// write the code here
+	vector<int> array = {10, 5, 2, 0, 7, 6, 4};
+	int n  = array.size() - 1;
+	int k = 4;
+	int ksm = kthsmallest(array, 0, n, k);
+
+		if (ksm != INT_MAX)
+			cout << "The Kth element :" << ksm;
+		else
+			cout << "Hence, value not found!!" << endl;
+
+	return 0;
+}
