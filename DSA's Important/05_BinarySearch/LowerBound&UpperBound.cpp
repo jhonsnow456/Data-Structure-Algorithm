3dd40f8ff9d932dd7dad06528af5dfb3c91bcd36
Aman Thakur <54764701+jhonsnow456@users.noreply.github.com>
2022-02-11 16:57:10 +0530

    Update LowerBound&UpperBound.cpp

--- DSA's Important/05_BinarySearch/LowerBound&UpperBound.cpp
+++ DSA's Important/05_BinarySearch/LowerBound&UpperBound.cpp
@@ -29,7 +29,7 @@ int lower_bound(vector<int> &v, int key){
 int upper_bound(vector<int> &v, int key){
 	int start = 0;
 	int end = v.size() - 1;
-	int ans = -1;
+	int ans = 0;
 
 		while (start <= end){
 			int mid = (start + end) / 2;
@@ -37,7 +37,7 @@ int upper_bound(vector<int> &v, int key){
 			if (v[mid] > key){
 				end = mid - 1;
 			}else{
-				ans = mid;
+				ans = mid + 1;
 				start = mid + 1;
 			}
 		}
@@ -49,8 +49,15 @@ int32_t main(){
 	// write the code here
 	vector<int> v = {0, 1, 1, 2, 3, 3, 3, 3, 4, 5, 5, 5, 16};
 	
-		cout << lower_bound(v, 3) << endl;
-		cout << upper_bound(v, 3) << endl;
+		// DEFINED BY ME
+		cout << "LOWER BOUND DEFINED by ME: " << lower_bound(v, -1) << endl;
+		cout << "UPPER BOUND DEFINED by ME: " << upper_bound(v, -1) << endl;
+		cout << "DIFFERENCE: " << upper_bound(v, -1) - lower_bound(v, -1) << endl;
 
+		// DEFINED BY STL
+		cout << "LOWER BOUND in STL: " << lower_bound(v.begin(), v.end(), -1) - v.begin() << endl;
+		cout << "UPPER BOUND in STL: " << upper_bound(v.begin(), v.end(), -1) - v.begin() << endl;
+		cout << "DIFFERENCE: " << upper_bound(v.begin(), v.end(), -1) - lower_bound(v.begin(), v.end(), -1) << endl;
+	
 	return 0;
 }
