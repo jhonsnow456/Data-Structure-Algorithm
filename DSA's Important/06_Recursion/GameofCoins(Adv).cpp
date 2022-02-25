83b9294802f511586b13a33117c184030fbaf0bd
Aman Thakur <54764701+jhonsnow456@users.noreply.github.com>
2022-02-26 03:58:57 +0530

    Update GameofCoins(Adv).cpp

--- DSA's Important/06_Recursion/GameofCoins(Adv).cpp
+++ DSA's Important/06_Recursion/GameofCoins(Adv).cpp
@@ -8,46 +8,44 @@
 using namespace std;
 
 int sum(vector<int> v, int row, int col){
-	int res = 0;
-
-		for (int k = row; k < col; k++)
-			res += v[k];
-	
-	return res;
-}
-
-int calc(vector<int> v, int start, int end, int k){
-	if (end - start + 1 < 0) return 0;
-
-	int res = INT_MIN;
-
-		for (int row = 0; row <= k; row++){
-			int ans = sum(v, start, start + row) + sum(v, end - k + row + 1, end + 1);
-			int op = INT_MAX;
-
-				for (int col = 0; col <= k; col++)
-					op = min(op, calc(v, start + row + col, end - k + row - k + col, k));
-
-			return max(res, ans + op);
-		}
-
-	return res;
-}
-
-int MaxValue(int N, vector<int> v, int k){
-    //Write your code here.
-    int res = calc(v, 0, N - 1, k);
+    int res = 0;
+    	for (int idx = row; idx < col; idx++) res += v[idx];
     return res;
 }
+
+int calc(vector<int> v, int start, int end, int idx){
+    if (end - start + 1 < 0)
+        return 0;
+
+    int res = INT_MIN;
+
+	    for (int row = 0; row <= idx; row++){
+	        int ans = sum(v, start, start + row) + sum(v, end - idx + row + 1, end + 1);
+	        int op = INT_MAX;
+	        
+	        for (int col = 0; col <= idx; col++)
+	            op = min(op, calc(v, start + row + col, end - idx + row - idx + col, idx));
+
+	        res = max(res, ans + op);
+	    }
+
+    return res;
+}
+
+int MaxValue(int N, vector<int> v, int idx){
+    int res = calc(v, 0, N - 1, idx);
+    return res;
+}
+
 int32_t main(){
 	// write the code here
-	int N, K; cin >> N >> K;
+	int N, idx; cin >> N >> idx;
 	vector<int> A(N);
 
 		for (int &val: A)
 			cin >> val;
 
-		cout << MaxValue(N, A, K) << endl;
+		cout << MaxValue(N, A, idx) << endl;
 
 	return 0;
 }
